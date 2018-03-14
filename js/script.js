e9d04a685d03fa6928ffded38905c81099143aa7
Louis Gabrie <louisgabrie1450@gmail.com>
2018-03-14 22:28:38 +0000

    I added the ajax function to the javascript

--- js/script.js
+++ js/script.js
@@ -2,9 +2,31 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
+/* global $ */
+
 $(document).ready(function(){
   
-  
+  $.ajax({
+  url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
+  method: "GET",
+  success: function(response) {
+        // Below log the response object to the console
+        //console.log(response);
+        //Log the data array to the console
+        //console.log(response.data);
+        //Log the first gif object from the data array
+        //console.log(response.data[0]);
+        //Log the images array to the console
+        //console.log(response.data[0].images);
+        //Log the orignal image to the console
+       //console.log(response.data[0].images.original);//       
+      //Log the url from the orignial gif to the console  
+      //console.log(response.data[0].images.original.url);
+        var api_url = response.data[0].images.original.url;
+        $('body').append('<img src=' + api_url + '>');
+        
+  },
+});
   
   
 });
