a4d5e54e80dfbc3c1804d8b877e318b61a6b94f1
Louis Gabrie <louisgabrie1450@gmail.com>
2018-04-18 21:18:05 +0000

    I made a gh-pages page

--- js/script.js
+++ js/script.js
@@ -5,11 +5,22 @@
 /* global $ */
 
 $(document).ready(function(){
-  
-  $.ajax({
-  url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
-  method: "GET",
-  success: function(response) {
+
+$('#submit').click(function() { 
+     var input = $("#srch-term").val();
+     console.log(input); 
+     
+  $.getJSON( 
+     "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=dc6zaTOxFJmzC",
+      
+     function(response) { 
+         console.log(response); 
+          $('body').append('<img src=' + response.data[0].images.fixed_width_downsampled.url + '>');
+     });
+   });
+});
+
+
         // Below log the response object to the console
         //console.log(response);
         //Log the data array to the console
@@ -19,14 +30,8 @@ $(document).ready(function(){
         //Log the images array to the console
         //console.log(response.data[0].images);
         //Log the orignal image to the console
-       //console.log(response.data[0].images.original);//       
-      //Log the url from the orignial gif to the console  
-      //console.log(response.data[0].images.original.url);
-        var api_url = response.data[0].images.original.url;
-        $('body').append('<img src=' + api_url + '>');
-        
-  },
-});
+       //console.log(response.data[0].images.original);     
+       //Log the url from the orignial gif to the console  
+       //console.log(response.data[0].images.original.url);
+      
   
-  
-});
